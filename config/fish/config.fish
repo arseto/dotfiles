ee6e2a8a8c18aeedfeaaf728580f008b0e0fe971
Arseto Nugroho <satriyo.796@gmail.com>
2015-12-25 17:10:58 +0700

    Added fish config and vim fugitive

---
+++ config/fish/config.fish
@@ -0,0 +1,10 @@
+alias ssh_cenil='ssh root@128.199.117.108'
+alias ssh_geblek='ssh root@128.199.98.78'
+alias ssh_ihik='ssh root@ihik.kulina.id'
+alias ssh_nfs='ssh root@128.199.155.206'
+alias ssh_tawon='ssh arseto@162.243.246.210'
+alias ssh_tawon_root='ssh root@162.243.246.210'
+alias ssh_uhui='ssh root@uhui.kulina.id'
+alias ssh_uhuk='ssh root@128.199.82.132'
+
+alias glc="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
