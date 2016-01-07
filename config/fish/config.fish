12907d3d5dc888b4a034c18017349ef20eb7d502
Arseto Nugroho <satriyo.796@gmail.com>
2016-01-07 14:25:14 +0700

    Some settings change
    * Added set numbering on gvim
    * Added default 'git push' branch
    * Added ssh and glc alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,3 +8,4 @@ alias ssh_uhui='ssh root@uhui.kulina.id'
 alias ssh_uhuk='ssh root@128.199.82.132'
 
 alias glc="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+set PATH /home/arseto/.gem/ruby/2.2.0/bin $PATH 
--- vimrc
+++ vimrc
@@ -70,6 +70,7 @@ if has('gui_running')
   set guioptions-=T  "remove toolbar
   set guioptions-=r  "remove right-hand scroll bar
   set guioptions-=L  "remove left-hand scroll bar
+  set nu
 endif
 
 " Syntastic php
