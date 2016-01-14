7e18eacf83311a113bddbf5dd2ebe606137e483d
Arseto Nugroho <satriyo.796@gmail.com>
2016-01-14 15:17:43 +0700

    Added $GOPATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,3 +9,5 @@ alias ssh_uhuk='ssh root@128.199.82.132'
 
 alias glc="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 set PATH /home/arseto/.gem/ruby/2.2.0/bin $PATH 
+
+set -x GOPATH $HOME/go
