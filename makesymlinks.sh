ee7f8b25292e1edd0a32588692a2448fa51ea884
Arseto Nugroho <satriyo.796@gmail.com>
2016-05-13 13:48:28 +0700

    Added gitignore and gitconfig symlink generator

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="tmux.conf vimrc vim gitconfig config/fish/config.fish gemrc"    # list of files/folders to symlink in homedir
+files="gitignore gitconfig tmux.conf vimrc vim gitconfig config/fish/config.fish gemrc"    # list of files/folders to symlink in homedir
 
 ##########
 
