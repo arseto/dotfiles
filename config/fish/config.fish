dcf87e105192bc3bd5de7321302c2140cbe60c4a
Arseto Nugroho <satriyo.796@gmail.com>
2016-06-07 03:19:41 +0700

    Added alias for nvim

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -21,6 +21,7 @@ alias gstat="git status"
 alias gch="git checkout"
 alias gbr="git branch"
 alias gac="git commit -am"
+alias nv="nvim"
 
 set PATH ~/.gem/ruby/1.9.1/bin ~/.gem/ruby/2.2.0/bin ~/.local/bin ~/.gem/ruby/2.3.0/bin ~/.npm-packages/bin/ ~/.composer/bin/ ~/.composer/vendor/bin/ ~/local/bin ~/.conscript/bin/ $PATH
 
